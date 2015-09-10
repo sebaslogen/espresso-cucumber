f423aeee689db9b8ef204b96dd7ba18bcf322594
Sebas LG <neoranga55@gmail.com>
2015-09-11 00:11:52 +0200

    CLean up

--- sample-test-project/FirstRunWizardEspressoTest/bin/FirstRunWizardEspressoTest.ap_.d
+++
@@ -1,2 +0,0 @@
-/home/sege/nav4-features/pndapps/Apps/FirstRunWizard/test/FirstRunWizardEspressoTest/bin/FirstRunWizardEspressoTest.ap_ \
- : /home/sege/nav4-features/pndapps/Apps/FirstRunWizard/test/FirstRunWizardEspressoTest/bin/AndroidManifest.xml \
--- sample-test-project/FirstRunWizardEspressoTest/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/sege/nav4-features/pndapps/Apps/FirstRunWizard/test/FirstRunWizardEspressoTest/bin/AndroidManifest.xml \
